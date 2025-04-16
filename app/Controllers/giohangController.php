7d4d8296ca2456c3af19fba3950f5d2452253aa0
J03Ngocanh <ngongocanh15072311@gmail.com>
2025-04-16 22:50:26 +0700

    update

--- app/Controllers/giohangController.php
+++ app/Controllers/giohangController.php
@@ -145,60 +145,58 @@ class giohangController extends Controller
 
             if ($result) {
                 if (!empty($_SESSION['giohang'])) {
+                    if ($_POST['phuong_thuc'] === 'vnpay_qr') {
+                        $vnp_Url = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
+                        $vnp_Returnurl = "https://inis-hvnh.site/inis/vnpay/return";
+                        $vnp_TmnCode = "Z0O9T9AJ"; // Mã TMN từ VNPAY
+                        $vnp_HashSecret = "NBWOGA7BHPKQ4IF59MXMPRJOFX1W9QQ5"; // Chuỗi bí mật từ VNPAY
+                        $vnp_TxnRef = $mahoadon; // Mã đơn hàng duy nhất
+                        $vnp_OrderInfo = $vnp_TxnRef;
+                        $vnp_Amount = $_POST['tong_thanhtoan'] * 100; // Nhân 100 vì VNPAY dùng đơn vị VNĐ * 100
+                        $vnp_Locale = 'vn';
+                        $vnp_BankCode = '';
+                        $vnp_IpAddr = $_SERVER['REMOTE_ADDR'];
+
+                        $inputData = array(
+                            "vnp_Version" => "2.1.0",
+                            "vnp_TmnCode" => $vnp_TmnCode,
+                            "vnp_Amount" => $vnp_Amount,
+                            "vnp_Command" => "pay",
+                            "vnp_CreateDate" => date('YmdHis'),
+                            "vnp_CurrCode" => "VND",
+                            "vnp_IpAddr" => $vnp_IpAddr,
+                            "vnp_Locale" => $vnp_Locale,
+                            "vnp_OrderInfo" => $vnp_OrderInfo,
+                            "vnp_OrderType" => "other",
+                            "vnp_ReturnUrl" => $vnp_Returnurl,
+                            "vnp_TxnRef" => $vnp_TxnRef
+                        );
+
+                        ksort($inputData);
+                        $query = "";
+                        $i = 0;
+                        $hashdata = "";
+                        foreach ($inputData as $key => $value) {
+                            if ($i == 1) {
+                                $hashdata .= '&' . urlencode($key) . "=" . urlencode($value);
+                            } else {
+                                $hashdata .= urlencode($key) . "=" . urlencode($value);
+                                $i = 1;
+                            }
+                            $query .= urlencode($key) . "=" . urlencode($value) . '&';
+                        }
+
+                        $vnp_Url .= "?" . $query;
+                        $vnpSecureHash = hash_hmac('sha512', $hashdata, $vnp_HashSecret);
+                        $vnp_Url .= 'vnp_SecureHash=' . $vnpSecureHash;
+
+                        header('Location: ' . $vnp_Url);
+                        exit();
+                    }
                     foreach ($_SESSION['giohang'] as $masanpham => $sanpham) {
                         $soluong = $sanpham['soluong'];
                         $giagoc = $sanpham['giagoc'];
                         $this->giohangModel->addOrderDetail($mahoadon, $masanpham, $soluong, $giagoc);
-
-                        if ($_POST['phuong_thuc'] === 'vnpay_qr') {
-                            $vnp_Url = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
-                            $vnp_Returnurl = "https://inis-hvnh.site/inis/vnpay/return";
-                            $vnp_TmnCode = "Z0O9T9AJ"; // Mã TMN từ VNPAY
-                            $vnp_HashSecret = "NBWOGA7BHPKQ4IF59MXMPRJOFX1W9QQ5"; // Chuỗi bí mật từ VNPAY
-                            $vnp_TxnRef = $mahoadon; // Mã đơn hàng duy nhất
-                            $vnp_OrderInfo = $vnp_TxnRef;
-                            $vnp_Amount = $_POST['tong_thanhtoan'] * 100; // Nhân 100 vì VNPAY dùng đơn vị VNĐ * 100
-                            $vnp_Locale = 'vn';
-                            $vnp_BankCode = '';
-                            $vnp_IpAddr = $_SERVER['REMOTE_ADDR'];
-
-                            $inputData = array(
-                                "vnp_Version" => "2.1.0",
-                                "vnp_TmnCode" => $vnp_TmnCode,
-                                "vnp_Amount" => $vnp_Amount,
-                                "vnp_Command" => "pay",
-                                "vnp_CreateDate" => date('YmdHis'),
-                                "vnp_CurrCode" => "VND",
-                                "vnp_IpAddr" => $vnp_IpAddr,
-                                "vnp_Locale" => $vnp_Locale,
-                                "vnp_OrderInfo" => $vnp_OrderInfo,
-                                "vnp_OrderType" => "other",
-                                "vnp_ReturnUrl" => $vnp_Returnurl,
-                                "vnp_TxnRef" => $vnp_TxnRef
-                            );
-
-                            ksort($inputData);
-                            $query = "";
-                            $i = 0;
-                            $hashdata = "";
-                            foreach ($inputData as $key => $value) {
-                                if ($i == 1) {
-                                    $hashdata .= '&' . urlencode($key) . "=" . urlencode($value);
-                                } else {
-                                    $hashdata .= urlencode($key) . "=" . urlencode($value);
-                                    $i = 1;
-                                }
-                                $query .= urlencode($key) . "=" . urlencode($value) . '&';
-                            }
-
-                            $vnp_Url .= "?" . $query;
-                            $vnpSecureHash = hash_hmac('sha512', $hashdata, $vnp_HashSecret);
-                            $vnp_Url .= 'vnp_SecureHash=' . $vnpSecureHash;
-
-                            header('Location: ' . $vnp_Url);
-                            exit();
-                        }
-
                         unset($_SESSION['giohang']);
                     }
                 }
